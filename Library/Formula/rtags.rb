4343150727683167164754f7022e3a0a5cec9bcb
Przemyslaw Kryger <pkryger@gmail.com>
2015-03-29 11:52:11 +0100

    even better llvm paths detections

--- Library/Formula/rtags.rb
+++ Library/Formula/rtags.rb
@@ -1,16 +1,36 @@
 class Rtags < Formula
-  def llvm_version;  if build.head?; '3.6' else '3.5' end                                      end
-  def llvm;          'llvm' + (llvm_version.delete  '.')                                       end
-  def llvm_bin;      Pathname.new("#{HOMEBREW_CELLAR}") + llvm + (llvm_version + build.head? ? '.0': '.1') + 'bin' end
-  def llvm_config;   llvm_bin + ('llvm-config-' + llvm_version)                                end
-  def clang;         llvm_bin + ('clang-' + llvm_version)                                      end
-  def clangxx;       llvm_bin + ('clang++-' + llvm_version)                                    end
+  def llvm_version
+    build.head? ? "3.6" : "3.5"
+  end
+
+  def llvm
+    "llvm" + (build.head? ? (llvm_version.delete ".") : "")
+  end
+
+  def llvm_bin
+    f = Formulary.factory llvm
+    if f.rack.directory?
+      kegs = f.rack.subdirs.map { |keg| Keg.new(keg) }.sort_by(&:version)
+      Pathname.new("#{kegs.last}") + "bin"
+    end
+  end
+
+  def llvm_config
+    llvm_bin + ("llvm-config-" + llvm_version)
+  end
+
+  def clang
+    llvm_bin + ("clang-" + llvm_version)
+  end
+
+  def clangxx
+    llvm_bin + ("clang++-" + llvm_version)
+  end
 
   homepage "https://github.com/Andersbakken/rtags"
 
   stable do
     url "https://github.com/Andersbakken/rtags/archive/v2.0.tar.gz"
-    version "2.0"
     sha256 "36733945ea34517903a0e5b800b06a41687ee25d3ab360072568523e5d610d6f"
 
     resource "rct" do
